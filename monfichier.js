b9e0a1dbfd5555481c2b0af4c6ad0d7bf44d4ece
Wailweb <wail.sdni@gmail.com>
2020-03-21 19:17:02 +0100

    first commit switch js

---
+++ monfichier.js
@@ -0,0 +1,13 @@
+var favoriteColor=  "red";
+
+switch(favoriteColor)
+{
+  case "blue":
+        console.log("Wouah! le bleu c'est trop beaux");
+        break;
+  case "red":
+        console.log("J'aimerais bien une voiture rouge!");
+        break;
+        default:
+        console.log("Je ne connais pas ta couleur");
+}
